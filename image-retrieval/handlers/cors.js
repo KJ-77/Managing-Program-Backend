fb73dd767036feaa065b4bd39e53b6a45c968f52
Khalil <kjamil2004@gmail.com>
2025-05-03 17:43:59 +0300

    Update cors.js

--- image-retrieval/handlers/cors.js
+++ image-retrieval/handlers/cors.js
@@ -7,9 +7,10 @@ exports.handler = async (event) => {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers":
-        "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,Cache-Control,Pragma,Origin,X-Requested-With",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS,PATCH",
       "Access-Control-Allow-Credentials": true,
+      "Access-Control-Max-Age": "1728000",
     },
     body: JSON.stringify({ message: "CORS enabled" }),
   };
